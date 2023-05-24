2691fda5a108259118688645fc4198ea77ca9b04
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-05-25 02:12:27 +0300

    done homeTask

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const saveTimeThrottled = throttle(function (currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000); // Обмеження викликів функції не частіше, ніж раз на секунду
+
+player.on('timeupdate', function (data) {
+  const currentTime = data.seconds;
+  saveTimeThrottled(currentTime);
+});
+
+// Відновлення відтворення зі збереженої позиції після перезавантаження сторінки
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  const parsedTime = parseFloat(savedTime);
+  player.setCurrentTime(parsedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+const saveFormStateThrottled = throttle(function () {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500); // Обмеження оновлення форми не частіше, ніж раз на 500 мілісекунд
+
+// Перевірка та заповнення полів форми при завантаженні сторінки
+const savedFormState = localStorage.getItem('feedback-form-state');
+if (savedFormState) {
+  const parsedFormState = JSON.parse(savedFormState);
+  emailInput.value = parsedFormState.email;
+  messageInput.value = parsedFormState.message;
+}
+
+// Відстеження події input на формі
+form.addEventListener('input', function () {
+  saveFormStateThrottled();
+});
+
+// Обробник події сабміту форми
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formState); // Виведення об'єкту з полями email та message у консоль
+
+  // Очищення сховища та полів форми
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+});
